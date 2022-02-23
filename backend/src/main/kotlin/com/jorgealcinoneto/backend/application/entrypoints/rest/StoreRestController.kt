9d8ec9f6aed0e9af482a8d63ec1f0bbf4227d3c6
Jorge Alcino Neto <jorgealcinoneto@gmail.com>
2022-02-23 20:15:19 -0300

    [feature/happy-way] - Cheking my rest controller and change backend server port.

--- backend/src/main/kotlin/com/jorgealcinoneto/backend/application/entrypoints/rest/StoreRestController.kt
+++ backend/src/main/kotlin/com/jorgealcinoneto/backend/application/entrypoints/rest/StoreRestController.kt
@@ -1,4 +1,15 @@
 package com.jorgealcinoneto.backend.application.entrypoints.rest
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("stores")
 class StoreRestController {
+
+    @GetMapping("/")
+    fun getStores(): String {
+        return "OK"
+    }
 }
