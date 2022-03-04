f7f174da6ee45256bdbbe87ed98746f1029e3bda
Jorge Alcino Neto <jorgealcinoneto@gmail.com>
2022-03-04 12:33:26 -0300

    [feature/consumers] - Changing test lib

--- backend/src/test/kotlin/com/jorgealcinoneto/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/com/jorgealcinoneto/backend/BackendApplicationTests.kt
@@ -3,6 +3,7 @@ package com.jorgealcinoneto.backend
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+
 @SpringBootTest
 class BackendApplicationTests {
 
